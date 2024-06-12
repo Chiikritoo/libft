ba6981d7d8adb7ace4438e9b161741114729bdad
Chiikritoo <anis.chikri27@gmail.com>
2024-06-12 23:18:07 +0200

    string functions

---
+++ srcs/string/ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anchikri <anchikri@student.42mulhouse.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 19:50:40 by anchikri          #+#    #+#             */
+/*   Updated: 2024/06/12 22:23:08 by anchikri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libs/libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	str = ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	return (str);
+}
---
+++ srcs/string/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anchikri <anchikri@student.42mulhouse.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 17:16:39 by anchikri          #+#    #+#             */
+/*   Updated: 2024/06/12 22:23:08 by anchikri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libs/libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = ft_strlen(s);
+	while (s[i] != (unsigned char)c)
+	{
+		if (i == 0)
+			return (0);
+		i--;
+	}
+	return ((char *) &s[i]);
+}
